dfb4c7130a787347934198d121fa9bd8a320032b
Chris Di Carlo <chris@dicarlosystems.ca>
2024-10-28 10:41:19 -0400

    Remove unnecessary skeleton files

--- src/LaravelDynamicQueueWorkers.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-namespace ChrisDiCarlo\LaravelDynamicQueueWorkers;
-
-class LaravelDynamicQueueWorkers {}
--- src/LaravelDynamicQueueWorkersServiceProvider.php
+++ src/LaravelDynamicQueueWorkersServiceProvider.php
@@ -10,16 +10,7 @@ class LaravelDynamicQueueWorkersServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
-            ->name('laravel-dynamic-queue-workers')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_laravel_dynamic_queue_workers_table')
-            ->hasCommand(LaravelDynamicQueueWorkersCommand::class);
+            ->name('laravel-dynamic-queue-workers');
     }
 }
